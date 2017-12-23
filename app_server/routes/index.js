a4403836b46624e0825ee4839c0bfa5a24a9fd73
dmitrus <nphoxconnect@gmail.com>
2017-12-23 22:47:23 +0400

    add about

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -12,4 +12,10 @@ var ctrlAuthorization = require('../controllers/authorization');
 router.get('/login', ctrlAuthorization.login);
 router.get('/register', ctrlAuthorization.register);
 
+var ctrlOthers = require('../controllers/others');
+
+router.get('/rules', ctrlOthers.rules);
+router.get('/about', ctrlOthers.about);
+
 module.exports = router;
+
